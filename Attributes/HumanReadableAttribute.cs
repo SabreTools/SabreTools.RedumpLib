fc4c17c0ee500ed0ef3962a58845775e362d139f
Matt Nadareski <mnadareski@outlook.com>
2023-09-04 16:30:28 -0400

    Add code from MPF

---
+++ Attributes/HumanReadableAttribute.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace SabreTools.RedumpLib.Attributes
+{
+    /// <summary>
+    /// Generic attribute for human readable values
+    /// </summary>
+    public class HumanReadableAttribute : Attribute
+    {
+        /// <summary>
+        /// Item is marked as obsolete or unusable
+        /// </summary>
+        public bool Available { get; set; } = true;
+
+        /// <summary>
+        /// Human-readable name of the item
+        /// </summary>
+        public string LongName { get; set; }
+
+        /// <summary>
+        /// Internally used name of the item
+        /// </summary>
+        public string ShortName { get; set; }
+    }
+}
---
+++ Converters/DiscTypeConverter.cs
@@ -0,0 +1,26 @@
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RedumpLib.Data;
+
+namespace SabreTools.RedumpLib.Converters
+{
+    /// <summary>
+    /// Serialize DiscType enum values
+    /// </summary>
+    public class DiscTypeConverter : JsonConverter<DiscType?>
+    {
+        public override bool CanRead { get { return false; } }
+
+        public override DiscType? ReadJson(JsonReader reader, Type objectType, DiscType? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, DiscType? value, JsonSerializer serializer)
+        {
+            JToken t = JToken.FromObject(value.LongName() ?? string.Empty);
+            t.WriteTo(writer);
+        }
+    }
+}
