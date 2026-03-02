f3fb98272c390b4d7f6d919b30f583638d9afd62
Matt Nadareski <mnadareski@outlook.com>
2026-03-02 00:23:47 -0500

    Add empty URI builder tests

---
+++ SabreTools.RedumpLib.Test/Web/UrlBuilderTests.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using SabreTools.RedumpLib.Web;
+using Xunit;
+
+namespace SabreTools.RedumpLib.Test.Web
+{
+    public class UrlBuilderTests
+    {
+        #region BuildDiscUrl
+
+        // TODO: Implement
+
+        #endregion
+
+        #region BuildDiscsUrl
+
+        // TODO: Implement
+
+        #endregion
+
+        #region BuildDiscsWipUrl
+
+        // TODO: Implement
+
+        #endregion
+
+        #region BuildDownloadsUrl
+
+        // TODO: Implement
+
+        #endregion
+
+        #region BuildListUrl
+
+        // TODO: Implement
+
+        #endregion
+
+        #region BuildMemberPromotionUrl
+
+        // TODO: Implement
+
+        #endregion
+
+        #region BuildNewDiscUrl
+
+        // TODO: Implement
+
+        #endregion
+
+        #region BuildPackUrl
+
+        // TODO: Implement
+
+        #endregion
+    }
+}
