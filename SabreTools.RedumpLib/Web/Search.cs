4e19eb2c68df1e3c11d77f2ea87a1a40380acefc
Matt Nadareski <mnadareski@outlook.com>
2026-03-02 21:56:14 -0500

    Reorder methods to match other extension classes

--- SabreTools.RedumpLib/Web/Search.cs
+++ SabreTools.RedumpLib/Web/Search.cs
@@ -10,54 +10,6 @@ namespace SabreTools.RedumpLib.Web
     public static class Search
     {
         /// <summary>
-        /// List the disc IDs associated with a given quicksearch query
-        /// </summary>
-        /// <param name="client">RedumpClient for connectivity</param>
-        /// <param name="query">Query string to attempt to search for</param>
-        /// <param name="convertForwardSlashes">Replace forward slashes with `-` in queries</param>
-        /// <param name="limit">Limit number of retrieved result pages, non-positive for unlimited</param>
-        /// <returns>All disc IDs for the given query, empty on error</returns>
-        public static async Task<List<int>> ListSearchResults(this RedumpClient client,
-            string? query,
-            bool convertForwardSlashes,
-            int limit = -1)
-        {
-            // If the query is invalid
-            if (string.IsNullOrEmpty(query))
-                return [];
-
-            // Keep getting quicksearch pages until there are none left
-            List<int> ids = [];
-            try
-            {
-                int pageNumber = 1;
-                while (true)
-                {
-                    if (limit > 0 && pageNumber >= limit)
-                        break;
-
-                    // Convert forward slashes implies a strict query
-                    var pageIds = convertForwardSlashes
-                        ? await client.CheckSingleDiscsPage(quicksearch: query, page: pageNumber++)
-                        : await client.CheckSingleQuicksearchPage(query!, pageNumber++);
-                    if (pageIds is null)
-                        return [];
-
-                    ids.AddRange(pageIds);
-                    if (pageIds.Count <= 1)
-                        break;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An exception occurred while trying to log in: {ex}");
-                return [];
-            }
-
-            return ids;
-        }
-
-        /// <summary>
         /// Download the disc pages associated with a given quicksearch query
         /// </summary>
         /// <param name="client">RedumpClient for connectivity</param>
@@ -95,6 +47,54 @@ namespace SabreTools.RedumpLib.Web
 
                     ids.AddRange(pageIds);
                     if (pageIds.Count == 0)
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception occurred while trying to log in: {ex}");
+                return [];
+            }
+
+            return ids;
+        }
+
+        /// <summary>
+        /// List the disc IDs associated with a given quicksearch query
+        /// </summary>
+        /// <param name="client">RedumpClient for connectivity</param>
+        /// <param name="query">Query string to attempt to search for</param>
+        /// <param name="convertForwardSlashes">Replace forward slashes with `-` in queries</param>
+        /// <param name="limit">Limit number of retrieved result pages, non-positive for unlimited</param>
+        /// <returns>All disc IDs for the given query, empty on error</returns>
+        public static async Task<List<int>> ListSearchResults(this RedumpClient client,
+            string? query,
+            bool convertForwardSlashes,
+            int limit = -1)
+        {
+            // If the query is invalid
+            if (string.IsNullOrEmpty(query))
+                return [];
+
+            // Keep getting quicksearch pages until there are none left
+            List<int> ids = [];
+            try
+            {
+                int pageNumber = 1;
+                while (true)
+                {
+                    if (limit > 0 && pageNumber >= limit)
+                        break;
+
+                    // Convert forward slashes implies a strict query
+                    var pageIds = convertForwardSlashes
+                        ? await client.CheckSingleDiscsPage(quicksearch: query, page: pageNumber++)
+                        : await client.CheckSingleQuicksearchPage(query!, pageNumber++);
+                    if (pageIds is null)
+                        return [];
+
+                    ids.AddRange(pageIds);
+                    if (pageIds.Count <= 1)
                         break;
                 }
             }
