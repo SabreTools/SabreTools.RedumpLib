5189d28cda4bda8094e1e26cb0c125c3e12e23c5
Matt Nadareski <mnadareski@outlook.com>
2026-06-14 19:53:06 -0400

    Fix build

--- SabreTools.RedumpLib/Validator.cs
+++ SabreTools.RedumpLib/Validator.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using SabreTools.RedumpLib.Data;
 using SabreTools.RedumpLib.RedumpOrg;
-using SabreTools.RedumpLib.RedumpOrg.Data;
 
 namespace SabreTools.RedumpLib
 {
