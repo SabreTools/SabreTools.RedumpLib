4c6fd26b22c4fef115ae18c689dfd41af8e055c8
Matt Nadareski <mnadareski@outlook.com>
2026-06-14 13:23:08 -0400

    Port Languages enum

--- SabreTools.RedumpLib.Test/RedumpInfo/ExtensionsTests.cs
+++ SabreTools.RedumpLib.Test/RedumpInfo/ExtensionsTests.cs
@@ -425,6 +425,175 @@ namespace SabreTools.RedumpLib.Test.RedumpInfo
 
         #endregion
 
+        #region Language
+
+        /// <summary>
+        /// Check that every Language has a long name provided
+        /// </summary>
+        /// <param name="language">Language value to check</param>
+        /// <param name="expectNull">True to expect a null value, false otherwise</param>
+        [Theory]
+        [MemberData(nameof(GenerateLanguageTestData))]
+        public void Language_LongName(Language? language, bool expectNull)
+        {
+            var actual = language.LongName();
+
+            if (expectNull)
+                Assert.Null(actual);
+            else
+                Assert.NotNull(actual);
+        }
+
+        /// <summary>
+        /// Check that every Language has a short name provided
+        /// </summary>
+        /// <param name="language">Language value to check</param>
+        /// <param name="expectNull">True to expect a null value, false otherwise</param>
+        [Theory]
+        [MemberData(nameof(GenerateLanguageTestData))]
+        public void Language_ShortName(Language? language, bool expectNull)
+        {
+            var actual = language.ShortName();
+
+            if (expectNull)
+                Assert.Null(actual);
+            else
+                Assert.NotNull(actual);
+        }
+
+        /// <summary>
+        /// Check that every Language can be mapped from a string
+        /// </summary>
+        /// <param name="lang">Language value to check</param>
+        /// <param name="expectNull">True to expect a null value, false otherwise</param>
+        [Theory]
+        [MemberData(nameof(GenerateLanguageTestData))]
+        public void Language_ToLanguage(Language? lang, bool expectNull)
+        {
+            string? twoLetterCode = lang.TwoLetterCode();
+            string? threeLetterCode = lang.ThreeLetterCode();
+            string? threeLetterCodeAlt = lang.ThreeLetterCodeAlt();
+
+            var actualTwoLetterCode = twoLetterCode.ToLanguage();
+            var actualThreeLetterCode = threeLetterCode.ToLanguage();
+            var actualThreeLetterCodeAlt = threeLetterCodeAlt.ToLanguage();
+
+            if (expectNull)
+            {
+                Assert.Null(actualTwoLetterCode);
+                Assert.Null(actualThreeLetterCode);
+                Assert.Null(actualThreeLetterCodeAlt);
+            }
+            else
+            {
+                Assert.Equal(lang, actualThreeLetterCode);
+
+                // Not guaranteed to exist
+                if (twoLetterCode is not null)
+                    Assert.Equal(lang, actualTwoLetterCode);
+                if (threeLetterCodeAlt is not null)
+                    Assert.Equal(lang, actualThreeLetterCodeAlt);
+            }
+        }
+
+        /// <summary>
+        /// Ensure that every Language that has a standard/bibliographic ISO 639-2 code is unique
+        /// </summary>
+        [Fact]
+        public void Language_ThreeLetterCode_NoDuplicates()
+        {
+            var fullLanguages = Enum.GetValues<Language>().Cast<Language?>().ToList();
+            var filteredLanguages = new Dictionary<string, Language?>();
+
+            int totalCount = 0;
+            foreach (Language? language in fullLanguages)
+            {
+                var code = language.ThreeLetterCode();
+                if (string.IsNullOrEmpty(code))
+                    continue;
+
+                // Throw if the code already exists
+                if (filteredLanguages.ContainsKey(code))
+                    throw new DuplicateNameException($"Code {code} already in dictionary");
+
+                filteredLanguages[code] = language;
+                totalCount++;
+            }
+
+            Assert.Equal(totalCount, filteredLanguages.Count);
+        }
+
+        /// <summary>
+        /// Ensure that every Language that has a terminology ISO 639-2 code is unique
+        /// </summary>
+        [Fact]
+        public void Language_ThreeLetterCodeAlt_NoDuplicates()
+        {
+            var fullLanguages = Enum.GetValues<Language>().Cast<Language?>().ToList();
+            var filteredLanguages = new Dictionary<string, Language?>();
+
+            int totalCount = 0;
+            foreach (Language? language in fullLanguages)
+            {
+                var code = language.ThreeLetterCodeAlt();
+                if (string.IsNullOrEmpty(code))
+                    continue;
+
+                // Throw if the code already exists
+                if (filteredLanguages.ContainsKey(code))
+                    throw new DuplicateNameException($"Code {code} already in dictionary");
+
+                filteredLanguages[code] = language;
+                totalCount++;
+            }
+
+            Assert.Equal(totalCount, filteredLanguages.Count);
+        }
+
+        /// <summary>
+        /// Ensure that every Language that has an ISO 639-1 code is unique
+        /// </summary>
+        [Fact]
+        public void Language_TwoLetterCode_NoDuplicates()
+        {
+            var fullLanguages = Enum.GetValues<Language>().Cast<Language?>().ToList();
+            var filteredLanguages = new Dictionary<string, Language?>();
+
+            int totalCount = 0;
+            foreach (Language? language in fullLanguages)
+            {
+                var code = language.TwoLetterCode();
+                if (string.IsNullOrEmpty(code))
+                    continue;
+
+                // Throw if the code already exists
+                if (filteredLanguages.ContainsKey(code))
+                    throw new DuplicateNameException($"Code {code} already in dictionary");
+
+                filteredLanguages[code] = language;
+                totalCount++;
+            }
+
+            Assert.Equal(totalCount, filteredLanguages.Count);
+        }
+
+        /// <summary>
+        /// Generate a test set of Language values
+        /// </summary>
+        /// <returns>MemberData-compatible list of Language values</returns>
+        public static TheoryData<Language?, bool> GenerateLanguageTestData()
+        {
+            var testData = new TheoryData<Language?, bool>() { { null, true } };
+            foreach (Language? language in Enum.GetValues<Language>().Cast<Language?>())
+            {
+                testData.Add(language, false);
+            }
+
+            return testData;
+        }
+
+        #endregion
+
         #region Media Type
 
         /// <summary>
---
+++ SabreTools.RedumpLib/RedumpInfo/Converters/LanguageConverter.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SabreTools.RedumpLib.RedumpInfo.Data;
+
+namespace SabreTools.RedumpLib.RedumpInfo.Converters
+{
+    /// <summary>
+    /// Serialize Language enum values
+    /// </summary>
+    public class LanguageConverter : JsonConverter<Language?[]>
+    {
+        public override bool CanRead { get { return true; } }
+
+        public override Language?[] ReadJson(JsonReader reader, Type objectType, Language?[]? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            // If we have a value already, don't overwrite it
+            if (hasExistingValue)
+                return existingValue ?? [];
+
+            // Get the current depth for checking
+            int currentDepth = reader.Depth;
+
+            // Read the array while it exists
+            List<Language> languages = [];
+            while (reader.Read() && reader.Depth > currentDepth)
+            {
+                if (reader.Value is not string value)
+                    continue;
+
+                Language? lang = value.ToLanguage();
+                if (lang is not null)
+                    languages.Add(lang.Value);
+            }
+
+            return [.. languages];
+        }
+
+        public override void WriteJson(JsonWriter writer, Language?[]? value, JsonSerializer serializer)
+        {
+            if (value is null)
+                return;
+
+            JArray array = [];
+            foreach (var val in value)
+            {
+                JToken t = JToken.FromObject(val.ShortName() ?? string.Empty);
+                array.Add(t);
+            }
+
+            array.WriteTo(writer);
+        }
+    }
+}
