81b5d4335aa481122629e01ec54e0e30827bea18
Matt Nadareski <mnadareski@outlook.com>
2026-06-23 12:21:21 -0400

    Remove TODO based on fixes

--- SabreTools.RedumpLib/Tools/Builder.cs
+++ SabreTools.RedumpLib/Tools/Builder.cs
@@ -52,7 +52,6 @@ namespace SabreTools.RedumpLib.Tools
         /// <param name="info">Existing SubmissionInfo object to fill</param>
         /// <param name="id">Redump disc ID to retrieve</param>
         /// <param name="includeAllData">True to include all pullable information, false to do bare minimum</param>
-        /// TODO: Validate that this code is working
         public static async Task<bool> FillFromId(Web.Client client, SubmissionInfo info, int id, bool includeAllData)
         {
             var discData = await client.DownloadSingleDiscPage(id);
